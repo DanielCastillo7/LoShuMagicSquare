ca449f815cdf748e0ea1184b9de32ccf4f84811e
Daniel Castillo <dcastillo2@cpp.edu>
2025-03-28 02:25:18 -0700

    Implemented random grid until success

--- lo_shu.c
+++ lo_shu.c
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int isMagicSquare(int grid[3][3]) {
     int sum = grid[0][0] + grid[0][1] + grid[0][2];
@@ -18,12 +20,35 @@ int isMagicSquare(int grid[3][3]) {
     return 1;
 }
 
-int main() {
-    int magicSquare[3][3] = {{4, 9, 2}, {3, 5, 7}, {8, 1, 6}};
-    int notMagicSquare[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+void generateRandomGrid(int grid[3][3]) {
+    int nums[9] = {1,2,3,4,5,6,7,8,9};
+    for (int i = 0; i < 9; i++) {
+        int j = rand() % 9;
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 
-    printf("Checking valid Magic Square: %s\n", isMagicSquare(magicSquare) ? "YES" : "NO");
-    printf("Checking invalid Magic Square: %s\n", isMagicSquare(notMagicSquare) ? "YES" : "NO");
+    int index = 0;
+    for (int r = 0; r < 3; r++)
+        for (int c = 0; c < 3; c++)
+            grid[r][c] = nums[index++];
+}
+
+int main() {
+    srand(time(0));
+    int grid[3][3], count = 0;
+
+    do {
+        generateRandomGrid(grid);
+        count++;
+    } while (!isMagicSquare(grid));
+
+    printf("Total attempts: %d\n", count);
+    printf("[%d %d %d]\n[%d %d %d]\n[%d %d %d]\n", 
+           grid[0][0], grid[0][1], grid[0][2], 
+           grid[1][0], grid[1][1], grid[1][2], 
+           grid[2][0], grid[2][1], grid[2][2]);
 
     return 0;
 }
